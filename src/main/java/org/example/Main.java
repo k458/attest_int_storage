6d6688bcbf66b0ee17588c735111a8f524b6a984
k458 <koreec499@gmail.com>
2025-02-05 20:22:44 +0900

    finish!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.Random;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -10,12 +12,26 @@ public class Main {
 
     public static void main(String[] args) {
         int rows = 3;
+        int totalCells = rows*rows;
         String field = generateField(rows);
         System.out.println("random field:\n" +field+ '\n');
         System.out.println("formatted field:\n" +formatFields(field, rows)+ '\n');
         int intToSave = convertToIntBits(field);
-        field = convertIntBitsToField(intToSave);
+        field = convertBinaryStringToField(convertIntToBinaryString(intToSave, totalCells));
         System.out.println("parsed field from " +intToSave+ ":\n" +formatFields(field, rows)+ '\n');
+        writeBinaryString(convertIntToBinaryString(intToSave, totalCells));
+    }
+
+    private static String convertIntToBinaryString(int input, int length){
+        String binaryString = Integer.toBinaryString(input);
+        sb.setLength(0);
+        int padding = length * 2 - binaryString.length();
+        for (int i = 0; i < padding; i++) {
+            sb.append('0');
+        }
+        sb.append(binaryString);
+        System.out.println("N: " +input+ ">>>>" +binaryString+ " , padding: " +padding);
+        return sb.toString();
     }
 
     private static String generateField(int rows){
@@ -81,8 +97,7 @@ public class Main {
         return ret;
     }
 
-    private static String convertIntBitsToField(int intBits){
-        String binaryString = Integer.toBinaryString(intBits);
+    private static String convertBinaryStringToField(String binaryString){
         sb.setLength(0);
         for (int i = 0; i < binaryString.length(); i = i + 2) {
             char ch = binaryString.charAt(i);
@@ -103,7 +118,39 @@ public class Main {
                 }
             }
         }
-        //System.out.println("N: parsed binary string " + binaryString + " that is equal to " + intBits+ " into ");
         return sb.toString();
+    }
+
+    private static void writeBinaryString(String binaryString){
+        String[] strings = new String[3];
+        sb.setLength(0);
+        for (int i = 0; i < 24; i++) {
+            char ch = '0';
+            if (i < binaryString.length()){
+                ch = binaryString.charAt(i);
+            }
+            sb.append(ch);
+            if ((i + 1) % 8 == 0){
+                strings[((i + 1) / 8) - 1] = sb.toString();
+                sb.setLength(0);
+            }
+        }
+        sb.setLength(0);
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println('\t' +strings[i]);
+        }
+
+        byte[] bytes = new byte[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(strings[i], 2);  // Convert binary string to byte
+        }
+
+        String path = "file3bytes";
+        try (FileOutputStream fos = new FileOutputStream(path)) {
+            // Write the byte array to the file
+            fos.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
